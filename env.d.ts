5a2041e46db5ec7b1cacd808ceb09b0baf5a9c2e
David <davidjs314@yahoo.com>
2024-10-30 11:34:34 -0400

    Add's secret for session to .env and logout route

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,7 @@ declare global {
     namespace NodeJS {
       interface ProcessEnv {
         DB_PASS: string;
+        SECRET: string;
       }
     }
   }
--- server.ts
+++ server.ts
@@ -17,8 +17,11 @@ app.use(cors({
     methods: ['GET', 'POST'],
     credentials: true // Optional, if you're handling cookies or authentication tokens
 }));
+
+const secret: string = process.env.SECRET;
+
 app.use(session({
-    secret: 'fillerfornow',
+    secret: secret,
     resave: false,
     saveUninitialized: false,
     cookie: {
